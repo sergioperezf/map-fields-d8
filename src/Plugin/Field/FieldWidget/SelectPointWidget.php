6b8efe038f91545e693acf6d5bfa6dc20a0867a8
Sergio Perez <sperez@H6452.local>
2017-01-30 16:41:35 -0500

    Adding class and starting lib

--- src/Plugin/Field/FieldWidget/SelectPointWidget.php
+++ src/Plugin/Field/FieldWidget/SelectPointWidget.php
@@ -6,12 +6,32 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Plugin implementation of the 'sperez_gmaps_point' widget.
+ *
+ * @FieldWidget(
+ *   id = "sperez_gmaps_select",
+ *   module = "sperez_maps_fields",
+ *   label = @Translation("Select a point in the map"),
+ *   field_types = {
+ *     "sperez_map_point"
+ *   }
+ * )
+ */
 class SelectPointWidget extends WidgetBase {
 
   /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-
+    $elements_array = [
+      'latitude' => [
+        '#type' =>'hidden'
+      ],
+      'longitude' => [
+        '#type' => 'hidden'
+      ]
+    ];
+    return $elements_array;
   }
 }
